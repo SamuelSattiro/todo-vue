0e75fb2739001a9edfe1b5da8ef8a2a983d4c98f
Samuel Sattiro <samuelsattiro.dev@gmail.com>
2025-09-29 12:17:34 -0300

    componentizando a aplicação

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,12 @@
+<script setup>
+    const props = defineProps( ['getTarefasPendentes'] );
+</script>
+
+<template>
+    <header class="p-5 mb-4 bg-light rounded-3">
+        <h1>Minhas tarefas</h1>
+        <p>
+            Você possui {{ getTarefasPendentes().length }} tarefas pendentes
+        </p>
+    </header>
+</template>
---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,24 @@
+<script setup>
+    const props = defineProps( ['getTarefasFiltradas'] )
+</script>
+
+<template>
+    <div>
+        <ul v-if="getTarefasFiltradas().length > 0" class="list-group mt-4">
+            <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
+                <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                    :id="tarefa.titulo" type="checkbox">
+                <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">{{ tarefa.titulo }}</label>
+            </li>
+        </ul>
+        <div v-else class="mt-4 text-center text-muted">Não há tarefas.</div>
+    </div>
+</template>
+
+<style scoped>
+
+.done {
+    text-decoration: line-through;
+}
+
+</style>
